8751d0da6c1d3c27b98ecba3510c201279df114f
Warren Lee <164605093+realwarrenlee@users.noreply.github.com>
2025-07-01 10:58:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'base: '/realwarrenlee.github.io/'',
+  base: 'base: '/'',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
